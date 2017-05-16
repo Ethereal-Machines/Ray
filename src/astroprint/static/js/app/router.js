db06efd2d56b9d1e53496d7ddff02143de3f0ebf
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-05-16 15:49:59 +0000

    Temperature control system is also working now

--- src/astroprint/static/js/app/router.js
+++ src/astroprint/static/js/app/router.js
@@ -188,7 +188,7 @@ var AppRouter = Backbone.Router.extend({
           */
 
           /* Comment-change#1 */
-          // this.controlView.tempView.resetBars();
+          this.controlView.tempView.resetBars();
         }
       }
 
