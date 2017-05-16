4834f2f2054317ac92920bd247a31de04694b24f
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-05-16 17:32:30 +0000

    Added comments to breakdown and code for understanding

--- src/astroprint/static/js/app/models/printfiles.js
+++ src/astroprint/static/js/app/models/printfiles.js
@@ -11,6 +11,7 @@ var PrintFile = Backbone.Model.extend({
   }
 });
 
+// when this method is being called, it's giving us the list of itmes from present in the local
 var PrintFileCollection = Backbone.Collection.extend({
   model: PrintFile,
   url: API_BASEURL + "astroprint/print-files",
