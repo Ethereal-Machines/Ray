590a74f668341d372588cfadd295fb6c6f13e7f8
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-05-16 10:01:19 +0000

    Added functionality to render the sub utility views

--- src/astroprint/static/js/app/app.js
+++ src/astroprint/static/js/app/app.js
@@ -12,7 +12,7 @@ $.ajaxSetup({
   }
 });
 
-console.log(UI_API_KEY);
+// console.log(UI_API_KEY);
 
 var AppMenu = Backbone.View.extend({
   el: '#main-menu',
@@ -60,47 +60,47 @@ var AstroBoxApp = Backbone.View.extend({
   initialize: function()
   {
     this.socketData = new SocketData();
-    console.log(this.socketData);
+    // console.log(this.socketData);
     
     /* AppMenu we are not including in the application */
     this.appMenu = new AppMenu();
-    console.log(this.appMenu);
+    // console.log(this.appMenu);
 
     /* 
     * This is corresponds to the 'utils.js' file .
     * This will be included in the new application
     */
     this.utils = new Utils();
-    console.log(this.utils);
+    // console.log(this.utils);
 
     /*
     * This the main property which is reponsible for
     * routing to the different applicaton pages of the application
     */
     this.router = new AppRouter();
-    console.log(this.router);
+    // console.log(this.router);
 
     /*
     * This is related to connections settings.
     * It will be included in the new app
     */
     this.connectionView = new ConnectionView({socket: this.socketData});
-    console.log(this.connectionView);
+    // console.log(this.connectionView);
 
     /*
     * This is the Modal for the printer profile
     * Will be there in the new app
     */
     this.printerProfile = new PrinterProfile(initial_printer_profile);
-    console.log(this.printerProfile);
+    // console.log(this.printerProfile);
 
     /*
     * This is responsible for 
     */
     this.eventManager = Backbone.Events;
-    console.log(this.eventManager);
+    // console.log(this.eventManager);
     this.socketData.connectionView = this.connectionView;
-    console.log(this.socketData.connectionView);
+    // console.log(this.socketData.connectionView);
     this.socketData.connect(WS_TOKEN);
     this.listenTo(this.socketData, 'change:printing', this.reportPrintingChange );
     this.listenTo(this.socketData, 'change:online', this.onlineStatusChange );
