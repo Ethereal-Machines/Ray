2d1aa1ce4607836be97283e6ffaf3ad35e70ffcc
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-05-16 09:40:38 +0000

    Refactored the code

--- src/astroprint/static/js/app/views/automatic-preheat.js
+++ src/astroprint/static/js/app/views/automatic-preheat.js
@@ -2,38 +2,11 @@
 * Code by Kanishka Mohan Madhuni *
 **********************************/
 
-var TempFilamentLoadPreheat = TempBarView.extend({
-  containerDimensions: null,
-  scale: null, // array of the values coming from the printer profile
-  type: null,
-  dragging: false,
-  // adding 'value' property to hold the object when the temperature updates
-  value: null,
-  events: _.extend(TempBarView.prototype.events, {
-    'click button.temp-off': 'turnOff'
-  }),
-  setHandle: function(value){
-    if (!this.dragging) {
-      var handle = this.$el.find('.temp-target');
-      handle.find('span.target-value').text(value);
-    }
-  },
-  renderTemps: function(actual, target){
-    if (actual !== null) {
-      this.$el.find('.current-temp-top').html(Math.round(actual)+'&deg;');
-    }
-    if (target !== null) {
-      this.setHandle(Math.min(Math.round(target), this.scale[1]));
-    }
-  },
-});
-
 var TempFilamentLoadPreheatView = Backbone.View.extend({
   el: '#filament-load-wizard__temp-control',
   nozzleTempBar: null,
-  initialize: function()
-  { 
-  	console.log("Yo Bro I am working fine yo");
+  initialize: function(){ 
+
     // creating the new instance for controlling the nozzle temp-bar
     this.nozzleTempBar = new TempBarVerticalView({
       scale: [0, app.printerProfile.get('max_nozzle_temp')],
@@ -49,19 +22,17 @@ var TempFilamentLoadPreheatView = Backbone.View.extend({
     this.nozzleTempBar.setMax(profile.max_nozzle_temp);
   },
   // this function is responsible for setting the nozzle and bed temperature
-  updateBars: function(value)
-  {
+  updateBars: function(value){
     if (value.extruder) {
       this.nozzleTempBar.setTemps(value.extruder.actual, value.extruder.target);
     }
-  }
+  },
 });
 
 var TempFilamentUnloadPreheatView = Backbone.View.extend({
   el: '#filament-unload-wizard__temp-control',
   nozzleTempBar: null,
   initialize: function(){ 
-  	console.log("Yo Bro I am working fine yo");
     // creating the new instance for controlling the nozzle temp-bar
     this.nozzleTempBar = new TempBarVerticalView({
       scale: [0, app.printerProfile.get('max_nozzle_temp')],
