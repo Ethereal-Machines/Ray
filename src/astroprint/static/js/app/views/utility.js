915d10a37804005215689614a9b5b143fb799e3f
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-05-16 18:07:40 +0000

    Added feature in to route the proper page based on the click

--- src/astroprint/static/js/app/views/utility.js
+++ src/astroprint/static/js/app/views/utility.js
@@ -14,6 +14,18 @@ var UtilityPage = Backbone.View.extend({
 });*/
 var UtilityView = Backbone.View.extend({
   el: '#utility-view',
+  events: {
+    "click .utility-button": "getButtonName"
+  },
   initialize: function() {},
-	onShow: function() {}
+	onShow: function() {},
+  getButtonName: function(e) {
+    // console.log("I am clicked");
+    // console.log(e.currentTarget);
+    var el = $(e.currentTarget);
+    var className = el.find('img').attr('class');
+    console.log(className);
+    ControlView.prototype.buttonName = className;
+
+  }
 });  
