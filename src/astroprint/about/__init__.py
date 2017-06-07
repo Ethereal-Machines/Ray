ed4fcbdd6a03acb03626f019dba347e66ef6fe8e
abinashk-inf <abinashkr94@gmail.com>
2017-06-07 17:14:04 +0530

    About Added

---
+++ src/astroprint/about/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+from more import index
+
+app = Flask(__name__)
+
+def info():
+    user = {'nickname': 'Praful', 'key': 'A154XWA256'}
+    posts = {'company': 'Ethereal Machines', 'link': 'http://www.etherealmachines.com/', 'firmware' : 'Marlin Firmware', 'version': 'v1.01'}
+    return render_template('index.html',  user=user, title= 'Try', posts=posts)   	
