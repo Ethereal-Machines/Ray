9a3b1d2d7ff45d6b9d34f982cf88bffad7d819d1
Vivek Anand <vivekanand1101@gmail.com>
2017-06-23 18:40:11 +0530

    Style fixes in variant and gcodeanalyser

--- src/astroprint/util/gCodeAnalyzer.py
+++ src/astroprint/util/gCodeAnalyzer.py
@@ -7,90 +7,99 @@ from sarge import run, Capture
 
 class GCodeAnalyzer(thread):
 
-	def __init__(self,filename,layersInfo,readyCallback,exceptionCallback,parent):
+    def __init__(self,filename,layersInfo,readyCallback,exceptionCallback,parent):
 
-		self._logger = logging.getLogger(__name__)
+        self._logger = logging.getLogger(__name__)
 
-		super(GCodeAnalyzer, self).__init__()
+        super(GCodeAnalyzer, self).__init__()
 
-		self.filename = filename
-		self.readyCallback = readyCallback
-		self.exceptionCallback = exceptionCallback
-		self.layersInfo = layersInfo
-		self.daemon = True
+        self.filename = filename
+        self.readyCallback = readyCallback
+        self.exceptionCallback = exceptionCallback
+        self.layersInfo = layersInfo
+        self.daemon = True
 
-		self.layerList = None
-		self.totalPrintTime = None
-		self.layerCount = None
-		self.size = None
-		self.layerHeight = None
-		self.totalFilament = None
-		self.parent = parent
+        self.layerList = None
+        self.totalPrintTime = None
+        self.layerCount = None
+        self.size = None
+        self.layerHeight = None
+        self.totalFilament = None
+        self.parent = parent
 
-	def makeCalcs(self):
-		self.start()
+    def makeCalcs(self):
+        self.start()
 
-	def run(self):
+    def run(self):
 
-		gcodeData = []
+        gcodeData = []
 
-		try:
-			pipe = run(
-				('%s/GCodeAnalyzer "%s" 1' if self.layersInfo else '%s/GCodeAnalyzer "%s"') % (
-					'/usr/bin/astroprint',
-					self.filename
-				), stdout=Capture())
+        try:
+            pipe = run(
+                ('%s/GCodeAnalyzer "%s" 1' if self.layersInfo else '%s/GCodeAnalyzer "%s"') % (
+                    '/usr/bin/astroprint',
+                    self.filename
+                ), stdout=Capture())
 
-			if pipe.returncode == 0:
-				try:
-					gcodeData = json.loads(pipe.stdout.text);
+            if pipe.returncode == 0:
+                try:
+                    gcodeData = json.loads(pipe.stdout.text);
 
-					if self.layersInfo:
-						self.layerList =  gcodeData['layers']
+                    if self.layersInfo:
+                        self.layerList =  gcodeData['layers']
 
-					self.totalPrintTime = gcodeData['print_time']
+                    self.totalPrintTime = gcodeData['print_time']
 
-					self.layerCount = gcodeData['layer_count']
+                    self.layerCount = gcodeData['layer_count']
 
-					self.size = gcodeData['size']
+                    self.size = gcodeData['size']
 
-					self.layerHeight = gcodeData['layer_height']
+                    self.layerHeight = gcodeData['layer_height']
 
-					self.totalFilament = None#total_filament has not got any information
+                    #total_filament has not got any information
+                    self.totalFilament = None
 
-					self.readyCallback(self.layerList,self.totalPrintTime,self.layerCount,self.size,self.layerHeight,self.totalFilament,self.parent)
-
-
-				except ValueError:
-					self._logger.error("Bad gcode data returned: %s" % pipe.stdout.text)
-					gcodeData = None
-
-					if self.exceptionCallback:
-						parameters = {}
-						parameters['parent'] = self.parent
-						parameters['filename'] = self.filename
-
-						self.exceptionCallback(parameters)
-
-			else:
-				self._logger.warn('Error executing GCode Analyzer')
-				gcodeData = None
+                    self.readyCallback(
+                        self.layerList,
+                        self.totalPrintTime,
+                        self.layerCount,
+                        self.size,
+                        self.layerHeight,
+                        self.totalFilament,
+                        self.parent
+                    )
 
 
-				if self.exceptionCallback:
-					parameters = {}
-					parameters['parent'] = self.parent
-					parameters['filename'] = self.filename
+                except ValueError:
+                    self._logger.error(
+                        "Bad gcode data returned: %s" % pipe.stdout.text)
+                    gcodeData = None
 
-					self.exceptionCallback(parameters)
+                    if self.exceptionCallback:
+                        parameters = {}
+                        parameters['parent'] = self.parent
+                        parameters['filename'] = self.filename
 
-		except:
-			gcodeData = None
+                        self.exceptionCallback(parameters)
 
-			if self.exceptionCallback:
-				parameters = {}
-				parameters['parent'] = self.parent
-				parameters['filename'] = self.filename
+            else:
+                self._logger.warn('Error executing GCode Analyzer')
+                gcodeData = None
 
-				self.exceptionCallback(parameters)
 
+                if self.exceptionCallback:
+                    parameters = {}
+                    parameters['parent'] = self.parent
+                    parameters['filename'] = self.filename
+
+                    self.exceptionCallback(parameters)
+
+        except:
+            gcodeData = None
+
+            if self.exceptionCallback:
+                parameters = {}
+                parameters['parent'] = self.parent
+                parameters['filename'] = self.filename
+
+                self.exceptionCallback(parameters)
--- src/astroprint/variant/__init__.py
+++ src/astroprint/variant/__init__.py
@@ -6,10 +6,10 @@ __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agp
 _instance = None
 
 def variantManager():
-	global _instance
-	if _instance is None:
-		_instance = VariantManager()
-	return _instance
+    global _instance
+    if _instance is None:
+        _instance = VariantManager()
+    return _instance
 
 import os
 import logging
@@ -17,18 +17,19 @@ import yaml
 
 from octoprint.settings import settings
 
-class VariantManager(object):	
-	def __init__(self):
-		self._settings = settings()
-		self._variantFile = self._settings.get(['software', 'variantFile']) or "%s/variant.yaml" % os.path.dirname(__file__)
-		self._logger = logging.getLogger(__name__)
+class VariantManager(object):
+    def __init__(self):
+        self._settings = settings()
+        self._variantFile = self._settings.get(['software', 'variantFile']) or \
+            "%s/variant.yaml" % os.path.dirname(__file__)
+        self._logger = logging.getLogger(__name__)
 
-		self.forceUpdateInfo = None
-		self.data = {}
+        self.forceUpdateInfo = None
+        self.data = {}
 
-		if not os.path.isfile(self._variantFile):
-			raise IOError("Variant File %s not found" % self._variantFile)
+        if not os.path.isfile(self._variantFile):
+            raise IOError("Variant File %s not found" % self._variantFile)
 
-		else:
-			with open(self._variantFile, "r") as f:
-				self.data = yaml.safe_load(f)
+        else:
+            with open(self._variantFile, "r") as f:
+                self.data = yaml.safe_load(f)
