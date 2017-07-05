5c84741d73d5eed4149fd727021ef3e5eaeffe63
Vivek Anand <vivekanand1101@gmail.com>
2017-07-05 13:19:53 +0530

    Some style fixes in utils/init
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/astroprint/util/__init__.py
+++ src/astroprint/util/__init__.py
@@ -3,44 +3,46 @@ import time
 from threading import Thread as thread
 
 
-
-def findProcess(processId ):
-    ps= subprocess.Popen("ps -ef | grep "+processId, shell=True, stdout=subprocess.PIPE)
+def findProcess(processId):
+    ''' Given the process id, find the name of the process '''
+    ps = subprocess.Popen(
+        "ps -ef | grep "+processId, shell=True, stdout=subprocess.PIPE)
     output = ps.stdout.read()
     ps.stdout.close()
     ps.wait()
     return output
 
 def isProcessRunning(processId):
-    
+    ''' Given the process id, check if it's running or not '''
     nameProcess = processId
-    
+
     lastChar = processId[len(processId)-1]
     processId = processId[:-1]
     processId = processId + '[' + lastChar + ']'
-    
-    output = findProcess( processId )
-    
+
+    output = findProcess(processId)
+
     if nameProcess in output:
         return True
     else:
         return False
 
+
 class interval(thread):
-        def __init__(self,period, callback,params=None):
-                self.time = period
-                self.originalCallback = callback
-                self.params = params or []
-                self.isRunning = None
-                thread.__init__(self)
-                self.daemon = True
+    def __init__(self,period, callback,params=None):
+        self.time = period
+        self.originalCallback = callback
+        self.params = params or []
+        self.isRunning = None
+        thread.__init__(self)
+        self.daemon = True
 
-        def run(self):
-                self.isRunning = True
-                while self.isRunning:
-                        time.sleep(self.time)
-                        if self.isRunning:
-                                self.originalCallback(*self.params)
+    def run(self):
+        self.isRunning = True
+        while self.isRunning:
+            time.sleep(self.time)
+            if self.isRunning:
+                self.originalCallback(*self.params)
 
-        def cancel(self):
-                self.isRunning = False
+    def cancel(self):
+        self.isRunning = False
