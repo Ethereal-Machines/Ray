1b35ff1dc4af1d566cb8faf06f0494c117437463
Vivek Anand <vivekanand1101@gmail.com>
2017-07-07 17:31:07 +0530

    Fix pep8 in about
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/astroprint/about/__init__.py
+++ src/astroprint/about/__init__.py
@@ -4,7 +4,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 def info():
-    user = {'nickname': 'Praful', 'key': 'A154XWA256'}
-    posts = {'company': 'Ethereal Machines', 'link': 'http://www.etherealmachines.com/', 'firmware' : 'Marlin Firmware', 'version': 'v1.01'}
-    return render_template('index.html',  user=user, title= 'Try', posts=posts)
+    user = {
+        'nickname': 'Praful',
+        'key': 'A154XWA256'
+    }
+    posts = {
+        'company': 'Ethereal Machines',
+        'link': 'http://www.etherealmachines.com/',
+        'firmware' : 'Marlin Firmware',
+        'version': 'v1.01'
+    }
+    return render_template(
+        'index.html',  user=user, title= 'Try', posts=posts)
