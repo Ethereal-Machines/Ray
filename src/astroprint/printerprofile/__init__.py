2b59de70201441cec66f3adb9f7b85c8f418a315
Vivek Anand <vivekanand1101@gmail.com>
2017-07-10 14:03:51 +0530

    Pep8 in printerprofiles
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/astroprint/printerprofile/__init__.py
+++ src/astroprint/printerprofile/__init__.py
@@ -6,11 +6,13 @@ __copyright__ = "Copyright (C) 2017 3DaGoGo, Inc - Released under terms of the A
 # singleton
 _instance = None
 
+
 def printerProfileManager():
-	global _instance
-	if _instance is None:
-		_instance = PrinterProfileManager()
-	return _instance
+    global _instance
+    if _instance is None:
+        _instance = PrinterProfileManager()
+    return _instance
+
 
 import os
 import yaml
@@ -19,69 +21,77 @@ import shutil
 
 from octoprint.settings import settings
 
+
 class PrinterProfileManager(object):
-	def __init__(self):
-		self._settings = settings()
+    def __init__(self):
+        self._settings = settings()
 
-		configDir = self._settings.getConfigFolder()
+        configDir = self._settings.getConfigFolder()
 
-		self._infoFile = "%s/printer-profile.yaml" % configDir
-		self._logger = logging.getLogger(__name__)
+        self._infoFile = "%s/printer-profile.yaml" % configDir
+        self._logger = logging.getLogger(__name__)
 
-		self.data = {
-			'driver': "marlin",
-			'extruder_count': 1,
-			'max_nozzle_temp': 280,
-			'max_bed_temp': 140,
-			'heated_bed': True,
-			'cancel_gcode': ['G28 X0 Y0'],
-			'invert_z': False
-		}
+        self.data = {
+            'driver': "marlin",
+            'extruder_count': 1,
+            'max_nozzle_temp': 280,
+            'max_bed_temp': 140,
+            'heated_bed': True,
+            'cancel_gcode': ['G28 X0 Y0'],
+            'invert_z': False
+        }
 
-		if not os.path.isfile(self._infoFile):
-			factoryFile = "%s/printer-profile.factory" % configDir
-			if os.path.isfile(factoryFile):
-				shutil.copy(factoryFile, self._infoFile)
-			else:
-				open(self._infoFile, 'w').close()
+        if not os.path.isfile(self._infoFile):
+            factoryFile = "%s/printer-profile.factory" % configDir
+            if os.path.isfile(factoryFile):
+                shutil.copy(factoryFile, self._infoFile)
+            else:
+                open(self._infoFile, 'w').close()
 
-		if self._infoFile:
-			config = None
-			with open(self._infoFile, "r") as f:
-				config = yaml.safe_load(f)
+        if self._infoFile:
+            config = None
+            with open(self._infoFile, "r") as f:
+                config = yaml.safe_load(f)
 
-			def merge_dict(a,b):
-				for key in b:
-					if isinstance(b[key], dict):
-						merge_dict(a[key], b[key])
-					else:
-						a[key] = b[key]
+            def merge_dict(a, b):
+                for key in b:
+                    if isinstance(b[key], dict):
+                        merge_dict(a[key], b[key])
+                    else:
+                        a[key] = b[key]
 
-			if config:
-				merge_dict(self.data, config)
+            if config:
+                merge_dict(self.data, config)
 
-	def save(self):
-		with open(self._infoFile, "wb") as infoFile:
-			yaml.safe_dump(self.data, infoFile, default_flow_style=False, indent="    ", allow_unicode=True)
+    def save(self):
+        with open(self._infoFile, "wb") as infoFile:
+            yaml.safe_dump(
+                self.data,
+                infoFile,
+                default_flow_style=False,
+                indent="    ",
+                allow_unicode=True
+            )
 
-	def set(self, changes):
-		for k in changes:
-			if k in self.data:
-				if self.data[k] != changes[k]:
-					if k == 'driver':
-						#change printer object
-						from astroprint.printer.manager import printerManager
+    def set(self, changes):
+        for k in changes:
+            if k in self.data:
+                if self.data[k] != changes[k]:
+                    if k == 'driver':
+                        # change printer object
+                        from astroprint.printer.manager import printerManager
+                        printerManager(changes['driver'])
 
-						printerManager(changes['driver'])
+                    self.data[k] = self._clean(k, changes[k])
+            else:
+                self._logger.error(
+                    "trying to set unkonwn printer profile field %s to %s" % \
+                    (k, str(changes[k])))
 
-					self.data[k] = self._clean(k, changes[k])
-			else:
-				self._logger.error("trying to set unkonwn printer profile field %s to %s" % (k, str(changes[k])))
-
-	def _clean(self, field, value):
-		if field in ['extruder_count', 'max_nozzle_temp', 'max_bed_temp']:
-			return int(value)
-		elif field == 'heated_bed':
-			return bool(value)
-		else:
-			return value
+    def _clean(self, field, value):
+        if field in ['extruder_count', 'max_nozzle_temp', 'max_bed_temp']:
+            return int(value)
+        elif field == 'heated_bed':
+            return bool(value)
+        else:
+            return value
