2eb50aa509255770aa1bd334b4c676bf1fb0214f
Vivek Anand <vivekanand1101@gmail.com>
2017-10-05 01:44:45 +0530

    Add different endpoint for print from usb
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/astroprint/api/files.py
+++ src/astroprint/api/files.py
@@ -352,6 +352,48 @@ def usb_file_info():
     return flask.jsonify(data)
 
 
+@api.route("/usbfiles/printfile")
+@api.route("/usbfiles/printfile/")
+def printFileCommand():
+    filepath = flask.request.args.get('futurepath')
+    if not os.path.exists(filepath)
+		return make_response("File not found %s" % (filepath), 404)
+
+	# valid file commands, dict mapping command name to mandatory parameters
+	valid_commands = {
+		"select": []
+	}
+
+	command, data, response = util.getJsonCommandFromRequest(request, valid_commands)
+	if response is not None:
+		return response
+
+	printer = printerManager()
+
+	if command == "select":
+		# selects/loads a file
+		printAfterLoading = False
+		if "print" in data.keys() and data["print"]:
+			if not printer.isOperational():
+				#We try at least once
+				printer.connect()
+
+				start = time.time()
+				connect_timeout = 5 #5 secs
+
+				while not printer.isOperational() and not printer.isClosedOrError() and time.time() - start < connect_timeout:
+					time.sleep(1)
+
+				if not printer.isOperational():
+					return make_response("The printer is not responding, can't start printing", 409)
+
+			printAfterLoading = True
+
+		sd = False
+		filenameToSelect = printer.fileManager.getAbsolutePath(filename)
+		printer.selectFile(filenameToSelect, sd, printAfterLoading)
+
+	return NO_CONTENT
 # @api.route("/files/printusb")
 # @api.route("/files/printusb/")
 # def print_from_usb():
