5cf8fd437a33a950cf67f3856cdd5f95ec955666
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-10-05 18:39:31 +0530

    Feature print from external storage --> Included Copy and Print feature through Modal (#81)
    Copy and print feature through modal

--- src/astroprint/static/js/app/views/printFromStorage.js
+++ src/astroprint/static/js/app/views/printFromStorage.js
@@ -39,8 +39,6 @@ var PrintFromStorageView = Backbone.View.extend({
     e.stopPropagation();
   },
   getExternalFileNames: function() {
-  	console.log("External storage options is being selected");
-
   	$.ajax({
   		url: '/api/usbfiles/usblist',
   		type: 'GET',
@@ -59,24 +57,19 @@ var ExternalStorageView = Backbone.View.extend({
 	el: "#external-storage-view",
 	fileList: null,
 	template: _.template($("#external-storage-file-list").html()),
-	events: {
-    'click .external-storage__file-name': 'copyFileToLocal'
-	},
 	printFileView: null,
 	list: null,
 	print_file: null,
+	copySuccessData: null,
 	initialize: function(params) {
 		if (params !== undefined) {
 			this.fileList = params;
-			console.log(params);
 		}
 
 		this.printFileView = new PrintFileView({
 			list: this.list,
 			print_file: this.print_file
 		});
-
-		console.log(this.printFileView);
 
 		this.render();
 
@@ -87,13 +80,33 @@ var ExternalStorageView = Backbone.View.extend({
 			this.$(".external-storage-wizard__files-list").html(this.template({
 				fileList: this.fileList
 			}));
+
+			this.delegateEvents({
+				'click .external-storage__file-name': 'openPrintModal',
+				'click .back-button--external-storage': 'hidePrintModal',
+				'click .copy--external-storage': 'copyFileToLocal',
+				'click .print-file--external-storage': 'printFile'
+			});
 		}
+	},
+	openPrintModal: function(e) {
+
+		e.preventDefault();
+
+		var target = $(e.target);
+		var targetName = target.text();
+
+		this.$(".external-stroage-wizard__confrim-print-modal").removeClass('hide');
+		this.$(".external-stroage-wizard__confrim-print-modal").find('.file-name').text(targetName);
+		this.$(".print-file--external-storage").css('pointer-events', 'none');
+
+	},
+	hidePrintModal: function() {
+		this.$(".external-stroage-wizard__confrim-print-modal").addClass('hide');
 	},
 	copyFileToLocal: function(e) {
 		e.preventDefault();
-		var target = $(e.target);
-		var targetName = target.text();
-		console.log(target.text());
+		var targetName = $(".file-name--external-storage").text();
 
 		var name, fullpath;
 
@@ -108,8 +121,6 @@ var ExternalStorageView = Backbone.View.extend({
 						filepath: fullpath
 					}
 
-					console.log(JSON.stringify(data));
-
 					var self = this;
 
 					$.ajax({
@@ -117,12 +128,13 @@ var ExternalStorageView = Backbone.View.extend({
 						method: "GET",
 						data: data,
 						success: function(data) {
-							console.log(data);
 
-							console.log(data.futurepath);
-							console.log(data.localFileName);
+							self.copySuccessData = data;
 
-							self.printFileView.printClicked(e, {filename: data.localFileName});
+							self.$(".print-info__no-details").text("Your File Copied Successfully!");
+							self.$(".copy--external-storage").css('pointer-events', 'none');
+							self.$(".print-file--external-storage").css('pointer-events', 'auto');
+
 						},
 						error: function(xhr) {
 							console.log(xhr);
@@ -132,5 +144,19 @@ var ExternalStorageView = Backbone.View.extend({
 			}
 		}
 
+	},
+	printFile: function(e) {
+		var self = this;
+		if (self.copySuccessData !== null) {
+			self.printFileView.printClicked(e, {filename: self.copySuccessData.localFileName});
+		} else {
+			this.$(".print-info__no-details").text("First copy the file in order to print.");
+			this.$(".print-file--external-storage").css('pointer-events', 'none');
+		}
+
+		this.$(".copy--external-storage").css('pointer-events', 'auto');
+		this.$(".print-file--external-storage").css('pointer-events', 'none');
+		this.$(".print-info__no-details").text("Analyzing G-code");
+		this.hidePrintModal();
 	}
 });
