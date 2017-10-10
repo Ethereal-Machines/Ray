11c076db17f4410773e2dd87ac5310825c95a36d
Vivek Anand <vivekanand1101@gmail.com>
2017-10-10 12:59:23 +0530

    Never save the filelist of external storage in settings
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/octoprint/settings.py
+++ src/octoprint/settings.py
@@ -9,6 +9,7 @@ import yaml
 import logging
 import re
 import shutil
+import copy
 
 APPNAME="AstroBox"
 
@@ -329,8 +330,10 @@ class Settings(object):
         if not self._dirty and not force:
             return
 
+        conf = copy.deepcopy(self._config)
+        conf['usb'] = default_settings['usb']
         with open(self._configfile, "wb") as configFile:
-            yaml.safe_dump(self._config, configFile, default_flow_style=False, indent="    ", allow_unicode=True)
+            yaml.safe_dump(conf, configFile, default_flow_style=False, indent="    ", allow_unicode=True)
             self._dirty = False
         self.load()
 
