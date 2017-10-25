abac74364cc16e83a47d49ea9f3ca3ab96182118
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-10-25 17:49:36 +0530

    change: Condition for preheating to be completed is changed

--- src/astroprint/static/js/app/views/preheat.js
+++ src/astroprint/static/js/app/views/preheat.js
@@ -50,7 +50,7 @@ var PreheatingView = Backbone.View.extend({
 	    this.updateProgressBar();
     }
 
-    if (this.extruderPercentage === 100 && this.extraPercentage === 100 && this.bedPercentage === 100) {
+    if (this.extruderPercentage >= 99 && this.extraPercentage >= 99 && this.bedPercentage >= 99) {
     	currentView.removeClass('active').addClass('hide');;
     	this.$("#preheat-wizard__finish-section").removeClass('hide').addClass('active');
     }
