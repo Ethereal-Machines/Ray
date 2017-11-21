5d36b87459218e9384374d57cf9c671cdb82b2a5
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-11-21 16:12:05 +0530

    feature: Added a new locked screen for EtherBox

--- src/astroprint/static/js/locked/locked.js
+++ src/astroprint/static/js/locked/locked.js
@@ -11,9 +11,11 @@ LoginForm = Backbone.View.extend({
   {
     e.preventDefault();
 
-    var loadingBtn = this.$('.loading-button');
+    var loadingIcon = this.$('.loading-icon');
 
-    loadingBtn.addClass('loading');
+    loadingIcon.removeClass('hide');
+
+    var self = this;
 
     $.ajax({
       type: 'POST',
@@ -25,6 +27,7 @@ LoginForm = Backbone.View.extend({
     })
     .done(function(){
       location.reload();
+      loadingIcon.addClass('hide');
     })
     .fail(function(xhr){
       var message = "Unkonwn error. Please refresh the page";
@@ -37,8 +40,11 @@ LoginForm = Backbone.View.extend({
         }
       }
 
-      noty({text: message , timeout: 3000});
-      loadingBtn.removeClass('loading');
+      var notifyView = new NotifyView({msg: message, type: 'warning'});
+      notifyView.$el.removeClass('hide');
+
+      loadingIcon.addClass('hide');
+      
     });
 
     return false;
@@ -62,6 +68,27 @@ LockedView = Backbone.View.extend({
           }
         })
     }, this), 3000);
+  }
+});
+
+var NotifyView = Backbone.View.extend({
+  el: "#notify-wizard",
+  template: _.template( $("#notify-template").html() ),
+  errorMsg: null,
+  type: null,
+  events: {
+    'click .next-button--notify': 'closeErrorMsg'
+  },
+  initialize: function(params) {
+    this.errorMsg = params.msg;
+    this.type = params.type;
+    this.render();
+  },
+  render: function() {
+    this.$el.html(this.template({msg: this.errorMsg, type: this.type}));
+  },
+  closeErrorMsg: function() {
+    this.$el.addClass('hide');
   }
 });
 
