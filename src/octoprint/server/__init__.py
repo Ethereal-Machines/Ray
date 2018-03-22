dcf757ae693bd20d23512e144a0ee2fd866a61a2
TORAN <toran.sahu@yahoo.com>
2018-03-22 11:22:26 +0530

    hardcoded UI_API_KEY for cross-origin solution

--- src/octoprint/server/__init__.py
+++ src/octoprint/server/__init__.py
@@ -444,7 +444,11 @@ class Server():
         if not s.getBoolean(['api', 'regenerate']) and s.getString(['api', 'key']):
             UI_API_KEY = s.getString(['api', 'key'])
         else:
-            UI_API_KEY = ''.join('%02X' % ord(z) for z in uuid.uuid4().bytes)
+            # UI_API_KEY = ''.join('%02X' % ord(z) for z in uuid.uuid4().bytes)
+
+            # commented above random UUID logic
+            # hardcoded to resolve api key dependency while cross-origin things
+            UI_API_KEY = 'E539C79C8AE0454689F2057B166BAD8E'
 
         # then initialize logging
         self._initLogging(self._debug, self._logConf)
