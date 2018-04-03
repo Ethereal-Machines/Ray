750aa6cd4ba8fbdd80f30df3ab2dc85cf612e643
TORAN <toran.sahu@yahoo.com>
2018-04-03 16:52:46 +0530

    [new] added new gcode analyzer for total_layers & print_time

---
+++ src/ethereal/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python2.7
+# coding="UTF-8"
+
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2018 Ethereal Machines - Released under terms of the AGPLv3 License"
